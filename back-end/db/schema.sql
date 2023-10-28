accb8e410b1c6e8fd933fd50c1ca754b3ff55f94
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2023-10-28 14:32:03 -0400

    meeting room and booking tables set upup next is testing them and queries and controllers set up

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -3,8 +3,20 @@ CREATE DATABASE meeting_room_dev;
 
 \c meeting_room_dev;
 
-CREATE TABLE users ();
+-- CREATE TABLE users ();
 
-CREATE TABLE meeting_room ();
+CREATE TABLE meeting_room (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    capacity VARCHAR(10) NOT NULL,
+    floor VARCHAR(10) NOT NULL
+);
 
-CREATE TABLE booking ();
+CREATE TABLE booking (
+    id SERIAL PRIMARY KEY,
+    room_id TEXT REFERENCES meeting_room(id),
+    meeting_name TEXT NOT NULL,
+    startDate TIMESTAMP NOT NULL,
+    endDate TIMESTAMP NOT NULL,
+    attendees TEXT
+);
