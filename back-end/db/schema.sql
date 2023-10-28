29ee9a3be1c63ac3867f05643ac7b6b44a322e20
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2023-10-27 22:10:26 -0400

    database files setup tables created in the schema database config file also created

---
+++ back-end/db/schema.sql
@@ -0,0 +1,10 @@
+DROP DATABASE IF EXISTS meeting_room_dev;
+CREATE DATABASE meeting_room_dev;
+
+\c meeting_room_dev;
+
+CREATE TABLE users ();
+
+CREATE TABLE meeting_room ();
+
+CREATE TABLE booking ();
