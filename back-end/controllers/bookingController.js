bbc82bb9afb060647ab1ce94dfe5447d904143ad
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2023-10-28 19:18:37 -0400

    booking queries and controller started

--- back-end/controllers/bookingController.js
+++ back-end/controllers/bookingController.js
@@ -1,7 +1,26 @@
 // CONFIGURATIONS FOR BOOKINGS
 const express = require("express");
-const booking = express.Router();
+const bookings = express.Router();
+
+// IMPORTING BOOKINGS QUERIES
+const {
+    getAllBookings
+} = require("../queries/booking.js");
+
+// BUILDING ROUTES
+
+// ALL BOOKINGS ROUTE // INDEX
+bookings.get("/", async (req, res) => {
+    const allBookings = await getAllBookings();
+    if (allBookings[0]) {
+        res.status(200).json(allBookings);
+    } else {
+        res.status(500).json({ error: "An Error occured while retrieving all bookings!" })
+    }
+})
+
+
 
 
 // EXPORTING BOOKINGS TO APP
-module.exports = booking;
+module.exports = bookings;
--- back-end/queries/booking.js
+++ back-end/queries/booking.js
@@ -1,5 +1,22 @@
 const db = require("../db/dbConfig.js");
 
+// QUERY TO GET ALL BOOKINGS
+const getAllBookings = async () => {
+    try {
+        console.log("listing all bookings");
+        const allBookings = await db.any(
+            "SELECT * FROM booking"
+        );
+        return allBookings;
+    } catch (error) {
+        return error;
+    }
+};
+
+
+
 
 // EXPORTING BOOKING QUERIES TO BOOKING CONTROLLER
-module.exports = {};
+module.exports = {
+    getAllBookings
+};
