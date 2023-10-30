0cf1c82d86bd2a686d6c9dd0629a965de75e62df
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2023-10-30 16:13:30 -0400

    populated db with some more data ready for deployment

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,11 +1,16 @@
 \c meeting_room_dev;
 
 INSERT INTO meeting_room (name, capacity, floor) VALUES 
+('Blackstone Boardroom', '40', '3'),
+('Blackstone Steakhouse', '125', '11'),
+('Pursuit Office', '100', '2'),
 ('Meeting Room 1', '6', '22'),
+('Great Hall', '30', '6'),
 ('Boardroom 2', '12', '21'),
 ('Hub', '30', '22');
 
 INSERT INTO booking (meeting_name, start_date, end_date, attendees, room_id) VALUES 
-('Team Alpha - Scrum Standup', '2023-10-29 10:30:00', '2023-10-29 12:50:00', 'addy12@gmail.com', '1'),
-('Star Team - Backlog Refinement', '2023-10-30 10:30:00', '2023-10-30 12:50:00', 'addy12@gmail.com', '2'),
-('Blackstone - General Meeting', '2023-10-31 10:30:00', '2023-10-31 12:50:00', 'addy12@gmail.com', '3');
+('Blackstone - General Meeting', '2023-11-03 17:30:00', '2023-11-03 18:00:00', 'abubakaradnanadams@pursuit.org', '1'),
+('Blackstone - Pursuit Interview', '2023-11-06 09:00:00', '2023-11-06 14:00:00', 'abubakaradnanadams@pursuit.org', '2'),
+('Team Alpha - Scrum Standup', '2023-11-07 10:30:00', '2023-11-07 12:50:00', 'abubakaradnanadams@pursuit.org', '3'),
+('Star Team - Backlog Refinement', '2023-11-08 10:30:00', '2023-11-08 12:50:00', 'abubakaradnanadams@pursuit.org', '4');
