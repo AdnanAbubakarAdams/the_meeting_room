53529d0a20e62b4b27486f2d8723a8079906b8b9
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2023-11-02 01:54:04 -0400

    login created and tested but will be tested further tomorrow

---
+++ front-end/src/components/mainNav/MainNav.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { Link, useNavigate } from 'react-router-dom'
+import { useAuth } from "../utils/AuthContext";
+import NavBar from '../navBar/NavBar';
+
+
+
+const MainNav = () => {
+    const navigate = useNavigate()
+    const {user, logoutUser} = useAuth()
+
+    // const logoutClick = () => {
+    //     navigate('/login')
+    // }
+
+  return (
+    <div className="header">
+        <div>
+            <Link id="header-logo" to="/">TMR</Link>
+        </div>
+
+        <div className="links--wrapper">
+            {user ? (
+            <>
+            <div className='navComp'>
+            <NavBar />
+            </div>
+                <Link to="/" className="header--link">Home</Link>
+                <Link to="/profile" className="header--link">Profile</Link>
+
+                <button onClick={logoutUser} className="btn">Logout</button>
+            </>
+            ):(
+ 
+                <Link className="btn" to="/login">Login</Link>
+
+            )}
+            
+        </div>
+    </div>
+  )
+}
+
+export default MainNav;
