243f2b2420d12d34fb214e6215d7c465bd24394d
Adnan Abubakar Adams <adnan_abubakaradams@yahoo.com>
2023-11-02 17:36:31 -0400

    added some little notes to the home page and also cleaned up

--- back-end/controllers/usersController.js
+++ back-end/controllers/usersController.js
@@ -1,7 +1,7 @@
 // CONFIGURATIONS FOR USERS
-const express = require("express");
-const users = express.Router();
+// const express = require("express");
+// const users = express.Router();
 
 
 // EXPORTING USERS TO APP
-module.exports = users;
+// module.exports = users;
